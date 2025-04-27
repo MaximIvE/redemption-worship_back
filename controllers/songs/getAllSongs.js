035676e9ac1f7a924daad3e65db09ac66d836a18
Maksym <preacherteacher8@gmail.com>
2025-04-27 21:21:07 +0300

    some changes forrun route

--- controllers/songs/getAllSongs.js
+++ controllers/songs/getAllSongs.js
@@ -1,17 +1,16 @@
 const { getGoogleDrive } = require("../../connect");
 const FOLDER_ID = process.env.GOOGLE_LYRICS_ID;
 
-const searchSettings = {
-  q: `'${FOLDER_ID}' in parents and trashed = false and name != 'ШАБЛОН.docx' and name contains '.docx'`,
-  fields: 'files(id, name)',
-};
 
 const getAll = async (req, res) => {
   console.log("api/songs");
 
   const googleDrive = getGoogleDrive();
   console.log("past drive");
-  const result = await googleDrive.files.list(searchSettings);
+  const result = await googleDrive.files.list({
+    q: `'${FOLDER_ID}' in parents and trashed = false and name != 'ШАБЛОН.docx' and name contains '.docx'`,
+    fields: 'files(id, name)',
+  });
   console.log("past result");
   const data = result.data.files;  
   console.log("past data");
