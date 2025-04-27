321788fb2762916d525f44b43b360556ad7dd6c9
Maksym <preacherteacher8@gmail.com>
2025-04-27 23:06:14 +0300

    filter of langauge for allSongs routeget all songs route

--- controllers/songs/getAllSongs.js
+++ controllers/songs/getAllSongs.js
@@ -1,21 +1,26 @@
 require('dotenv').config();
 const { getGoogleDrive } = require("../../connect");
+const { separateOfName } = require('../../helpers');
 const FOLDER_ID = process.env.GOOGLE_LYRICS_ID;
 
 
 const getAll = async (req, res) => {
   console.log("api/songs");
+  const { lang = "all" } = req.query;
 
   const googleDrive = getGoogleDrive();
-  console.log("past drive");
+
   const result = await googleDrive.files.list({
     q: `'${FOLDER_ID}' in parents and trashed = false and name != 'ШАБЛОН.docx' and name contains '.docx'`,
     fields: 'files(id, name)',
   });
-  console.log("past result");
+
   const data = result.data.files;  
-  console.log("past data");
-  res.json(data);
+
+  if (!data || !data.length) throw RequestError(500);
+  const newData = data.map(song => separateOfName(song)).filter(song => (lang==='all'? true : song.textLang === lang))
+
+  res.json(newData);
 }
 
 module.exports = getAll;
