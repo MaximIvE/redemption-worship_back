84410d9dd7c3fa49fc75f4ca71be99971dd32bdb
Maksym <preacherteacher8@gmail.com>
2025-05-02 23:56:05 +0300

    connect MongoDm

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
-require('dotenv').config();
 const app = require('./app');
 const { initDrive } = require('./connect');
+const mongoose = require('mongoose');
+require('dotenv').config();
 
 
-
-const { PORT = 4000 } = process.env;
+const { MONGO_DB_HOST, PORT = 4000 } = process.env;
 
 function startServer(){
     initDrive();
-    app.listen(PORT, console.log(`Server success running at the port ${PORT}`));
+    mongoose.connect(MONGO_DB_HOST)
+        .then(() => {
+            console.log("Database MongoDB connect success");
+            app.listen(PORT, console.log(`Server success running at the port ${PORT}`));
+        })
+        .catch(error => {
+            console.log(error.message);
+            process.exit(1);
+        });
+
 };
 
 startServer();
