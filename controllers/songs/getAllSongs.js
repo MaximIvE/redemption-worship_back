d9dc5276dc597b344943d133452c09ca5f562bbe
Maksym <preacherteacher8@gmail.com>
2025-08-27 12:13:15 +0300

    a search query for lyrics and artists has been added to the endpoint for retrieving all songs

--- controllers/songs/getAllSongs.js
+++ controllers/songs/getAllSongs.js
@@ -3,16 +3,51 @@ const {Song} = require("../../models/song");
 
 
 const getAll = async (req, res) => {
-const {search=""} = req.query;
+const {search=""} = req.query; // Пошуковий запит (фільтр) по текстам, виконавцям
 
-const data = await Song.find(
+const data = await Song.aggregate([
+    {$unwind: {path: "$lyrics", preserveNullAndEmptyArrays: true}},
+    {$unwind: {path: "$lyrics.lines", preserveNullAndEmptyArrays: true}},
+
     {
-        $or: [
-            {"lyrics.lines.text": {$regex: search, $options: "i"}},
-            {artist: {$regex: search, $options: "i"}}
-        ]
+        $match:{
+            $or:[
+                {artist: {$regex: search, $options: "i"}},
+                {"lyrics.lines.text": {$regex: search, $options: "i"}}
+            ]
+        }
+    },
+
+    {
+        $group:{
+            _id: "$song_id",
+            title: {$first: "$title"},
+            title_en: {$first: "$title_en"},
+            language: {$first: "$language"},
+            search: {
+                $first: {
+                    $switch:{
+                        branches: [
+                            {case: {$regexMatch: {input: "$lyrics.lines.text", regex: search, options: "i"}}, then: "$lyrics.lines.text"},
+                            {case: {$regexMatch: {input: "$artist", regex: search, options: "i"}}, then: "$artist"}
+                        ]
+                    }
+            }
+        }}
+    },
+
+    {
+        $project:{
+            _id: 0,
+            song_id: "$_id",
+            title: 1,
+            title_en: 1, 
+            language: 1,
+            search: 1
+        }
     }
-).select('song_id title title_en language -_id');
+]);
+
 
 if(!data.length) throw RequestError(404);
 const sortData = sortByTitle(data);
